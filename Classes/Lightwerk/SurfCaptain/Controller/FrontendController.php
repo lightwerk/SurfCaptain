0aa47ff75b443f393cbff91d92ade9ec48982952
Lars Peipmann <lp@lightwerk.com>
2014-07-01 23:32:49 +0200

    [FEATURE] Adds FrontendController

---
+++ Classes/Lightwerk/SurfCaptain/Controller/FrontendController.php
@@ -0,0 +1,19 @@
+<?php
+namespace Lightwerk\SurfCaptain\Controller;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "Lightwerk.SurfCaptain". *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+
+class FrontendController extends \TYPO3\Flow\Mvc\Controller\ActionController {
+
+	/**
+	 * @return void
+	 */
+	public function indexAction() {
+	}
+
+}
