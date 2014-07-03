97a85fce93f90501239ebb10146347dc8faba738
Daniel Goerz <dlg@lightwerk.com>
2014-07-03 23:12:14 +0200

    [BUGFIX] removed jasmine tests from concatination

--- grunt/concat.js
+++ grunt/concat.js
@@ -10,7 +10,8 @@ module.exports = {
             // First include instantiation of new angular model
             'Resources/Public/Scripts/SurfCaptainApp/SurfCaptainApp.js',
             // and after that the rest
-            'Resources/Public/Scripts/SurfCaptainApp/**/*.js'
+            'Resources/Public/Scripts/SurfCaptainApp/**/*.js',
+            '!Resources/Public/Scripts/SurfCaptainApp/Tests/**/*.js'
         ],
         dest: 'Resources/Public/Scripts/Main.js'
     }
