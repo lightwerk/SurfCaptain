e91a2e55e0ca038987d15239842053a96f3d670c
Daniel Goerz <dlg@lightwerk.com>
2014-07-10 20:01:56 +0200

    [TASK] reder about view with angular and added more technologies

--- Resources/Public/Scripts/SurfCaptainApp/Controllers/AboutController.js
+++ Resources/Public/Scripts/SurfCaptainApp/Controllers/AboutController.js
@@ -3,5 +3,105 @@
 
 'use strict';
 surfCaptain.controller('AboutController', ['$scope', function ($scope) {
-
+    $scope.techs = [
+        {
+            name: 'angular',
+            url: 'https://angularjs.org/',
+            description: {
+                headline: 'AngularJS',
+                span1: 'JavaScript',
+                span2: 'Framework'
+            }
+        },
+        {
+            name: 'flow',
+            url: 'http://flow.typo3.org/',
+            description: {
+                headline: 'TYPO3 FLOW',
+                span1: 'PHP-Application',
+                span2: 'Framework'
+            }
+        },
+        {
+            name: 'bootstrap',
+            url: 'http://getbootstrap.com/',
+            description: {
+                headline: 'Bootstrap',
+                span1: 'CSS',
+                span2: 'Framework'
+            }
+        }
+    ];
+    $scope.subtechs = [
+        {
+            name: 'grunt',
+            url: 'http://gruntjs.com/',
+            description: {
+                headline: 'Grunt'
+            }
+        },
+        {
+            name: 'bower',
+            url: 'http://bower.io/',
+            description: {
+                headline: 'Bower'
+            }
+        },
+        {
+            name: 'composer',
+            url: 'https://getcomposer.org/',
+            description: {
+                headline: 'Composer'
+            }
+        },
+        {
+            name: 'karma',
+            url: 'http://karma-runner.github.io',
+            description: {
+                headline: 'Karma'
+            }
+        },
+        {
+            name: 'jasmine',
+            url: 'http://jasmine.github.io/',
+            description: {
+                headline: 'Jasmine'
+            }
+        },
+        {
+            name: 'jquery',
+            url: 'http://jquery.com/',
+            description: {
+                headline: 'jQuery'
+            }
+        },
+        {
+            name: 'css3',
+            url: 'http://en.wikipedia.org/wiki/Cascading_Style_Sheets#CSS_3',
+            description: {
+                headline: 'CSS 3'
+            }
+        },
+        {
+            name: 'git',
+            url: 'http://git-scm.com/',
+            description: {
+                headline: 'git'
+            }
+        },
+        {
+            name: 'html5',
+            url: 'http://en.wikipedia.org/wiki/HTML5',
+            description: {
+                headline: 'HTML 5'
+            }
+        },
+        {
+            name: 'mysql',
+            url: 'http://www.mysql.com/',
+            description: {
+                headline: 'MySQL'
+            }
+        }
+    ];
 }]);
