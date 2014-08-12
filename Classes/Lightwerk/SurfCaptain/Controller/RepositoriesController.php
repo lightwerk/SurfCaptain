303868926296c3bdddb0fdedd2698041512c1961
Lars Malach <lp@lightwerk.com>
2014-08-12 09:33:31 +0200

    [TASK] Adds identifier to repository response

--- Classes/Lightwerk/SurfCaptain/Controller/RepositoriesController.php
+++ Classes/Lightwerk/SurfCaptain/Controller/RepositoriesController.php
@@ -18,17 +18,25 @@ class RepositoriesController extends AbstractRestController {
 	protected $gitService;
 
 	/**
+	 * @var array
+	 */
+	protected $identifiers = array();
+
+	/**
 	 * @return void
 	 */
 	public function listAction() {
 		try {
 			$tempRepositories = $this->gitService->getRepositories();
 			$repositories = array();
+			$this->clearIdentifiers();
 			foreach ($tempRepositories as $tempRepository) {
 				$repositories[] = array(
 					'repository_url' => $tempRepository['ssh_url_to_repo'],
 					'name' => $tempRepository['name'],
+					'path_with_namespace' => $tempRepository['path_with_namespace'],
 					'web_url' => $tempRepository['web_url'],
+					'identifier' => $this->getIdentifier($tempRepository['name']),
 				);
 			}
 			$this->view->assign('repositories', $repositories);
@@ -37,5 +45,27 @@ class RepositoriesController extends AbstractRestController {
 		} catch (\TYPO3\Flow\Http\Exception $e) {
 			$this->handleException($e);
 		}
+	}
+
+	/**
+	 * @param string $identifier
+	 * @return string
+	 */
+	protected function getIdentifier($identifier) {
+		while (in_array($identifier, $this->identifiers)) {
+			if (preg_match('/^(.*)([0-9]+)$/', $identifier, $matches)) {
+				$identifier = $matches[1] . ((integer) $matches[2] + 1);
+			} else {
+				$identifier .= 1;
+			}
+		}
+		return $identifier;
+	}
+
+	/**
+	 * @return void
+	 */
+	protected function clearIdentifiers() {
+		$this->identifiers = array();
 	}
 }
