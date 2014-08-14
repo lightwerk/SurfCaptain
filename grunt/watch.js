b5981c76b1a9f76a1374ca7758774fca15b7d737
Daniel Goerz <dlg@lightwerk.com>
2014-08-14 16:35:03 +0200

    [TASK] followup on change of body color

--- grunt/watch.js
+++ grunt/watch.js
@@ -3,7 +3,7 @@
 'use strict';
 module.exports = {
     stylesheets: {
-        files: ['Resources/Public/Stylesheets/SurfCaptain.less'],
+        files: ['Resources/Public/Stylesheets/*.less'],
         tasks: ['less'],
         options: {
             nospawn: true
