cd9d2128f649acaabcb790877c465b6f50640ff4
Lars Malach <lp@lightwerk.com>
2014-08-15 23:15:07 +0200

    [TASK] Removes ConfigurationManager

--- Classes/Lightwerk/SurfCaptain/Controller/FrontendSettingController.php
+++ Classes/Lightwerk/SurfCaptain/Controller/FrontendSettingController.php
@@ -6,21 +6,14 @@ namespace Lightwerk\SurfCaptain\Controller;
  *                                                                        *
  *                                                                        */
 
-use Lightwerk\SurfCaptain\Configuration\ConfigurationManager;
 use TYPO3\Flow\Annotations as Flow;
 
 class FrontendSettingController extends AbstractRestController {
 
 	/**
-	 * @var ConfigurationManager
-	 * @FLOW\Inject
-	 */
-	protected $configurationManager;
-
-	/**
 	 * @return void
 	 */
 	public function listAction() {
-		$this->view->assign('frontendSettings', $this->configurationManager->getFrontendSettings());
+		$this->view->assign('frontendSettings', $this->settings['frontendSettings']);
 	}
 }
