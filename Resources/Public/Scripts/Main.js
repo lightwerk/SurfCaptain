95614bc69854d7d3f67da52eaf058cf89256be0a
Daniel Goerz <dlg@lightwerk.com>
2014-10-11 22:51:10 +0200

    [TASK] removed superluous FlashMessageService

--- Resources/Public/Scripts/Main.js
+++ Resources/Public/Scripts/Main.js
@@ -2587,54 +2587,6 @@ angular.module('surfCaptain').directive('overlay', function () {
     'use strict';
     angular
         .module('surfCaptain')
-        .service('FlashMessageService', FlashMessageService);
-
-    /* @ngInject */
-    function FlashMessageService() {
-        var messages = [];
-
-        /**
-         *
-         * @param {string} title
-         * @param {string} message
-         * @param {integer} severity
-         * @param {string} id
-         * @return {Array}
-         */
-        this.addFlashMessage = function (title, message, severity, id) {
-            messages.push({
-                title: title || '',
-                message: message || '',
-                severity: severity,
-                time: new Date(),
-                id: id
-            });
-            return messages;
-        };
-
-        /**
-         * @return {Array}
-         */
-        this.getFlashMessages = function () {
-            return messages;
-        };
-
-        /**
-         * Resets the messages to an empty Array
-         *
-         * @return {void}
-         */
-        this.flush = function () {
-            messages = [];
-        };
-    }
-}());
-/* global angular */
-
-(function () {
-    'use strict';
-    angular
-        .module('surfCaptain')
         .service('MarkerService', MarkerService);
 
     /* @ngInject */
