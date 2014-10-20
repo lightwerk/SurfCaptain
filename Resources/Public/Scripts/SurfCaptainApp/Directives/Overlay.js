bd68b18c3a9328d33491030dadca219d8e5835e9
Daniel Goerz <dlg@lightwerk.com>
2014-10-20 21:22:16 +0200

    [BUGFIX] removed unneeded code from overlay directive.

--- Resources/Public/Scripts/SurfCaptainApp/Directives/Overlay.js
+++ Resources/Public/Scripts/SurfCaptainApp/Directives/Overlay.js
@@ -8,15 +8,12 @@
 
     /* @nInject */
     function overlay() {
-        var linker = function () {};
-
         return {
             restrict: 'E',
             template: '<div data-ng-class="{false:\'overlay\'}[finished]"></div>',
             scope: {
                 finished: '='
-            },
-            link: linker
+            }
         };
     }
 }());
