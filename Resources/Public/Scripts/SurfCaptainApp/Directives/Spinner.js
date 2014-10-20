59ff45ad48df193abe2fd8921ce5ebcd67f7bf0f
Daniel Goerz <dlg@lightwerk.com>
2014-10-20 21:20:50 +0200

    [BUGFIX] removed unneeded code from spinner directive.

--- Resources/Public/Scripts/SurfCaptainApp/Directives/Spinner.js
+++ Resources/Public/Scripts/SurfCaptainApp/Directives/Spinner.js
@@ -8,13 +8,9 @@
 
     /* @ngInject */
     function spinner() {
-        // scope, element, attrs are passed to the linker function but not needed here
-        function linker() {}
-
         return {
             restrict: 'E',
-            template: '<i class="fa fa-spinner fa-spin fa-4x"></i>',
-            link: linker
+            template: '<i class="fa fa-spinner fa-spin fa-4x"></i>'
         };
     }
 }());
