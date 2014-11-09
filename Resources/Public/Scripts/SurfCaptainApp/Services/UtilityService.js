ef2e7033ff8dba9a2181abfdbb1217488421c6e7
Daniel Goerz <dlg@lightwerk.com>
2014-11-09 11:14:51 +0100

    [TASK] shorten the sha1 hash on currently deployed commit information and added unittests for UtilityService.

--- Resources/Public/Scripts/SurfCaptainApp/Services/UtilityService.js
+++ Resources/Public/Scripts/SurfCaptainApp/Services/UtilityService.js
@@ -7,33 +7,43 @@
         .service('UtilityService', UtilityService);
 
     /* @ngInject */
-    function UtilityService() {
+    function UtilityService($filter) {
 
         /**
+         * Searches within an Array of commits for a commit with a certain name
+         * to return information about the commit, containing
+         *   * Whether it was a Tag, a Branch or a standalone commit (sha1)
+         *   * Name of the committer
+         *   * Commit message
          *
          * @param {string} name
-         * @param {array} tags
+         * @param {Array} commits
          * @returns {string}
          */
-        this.getDeployedTag = function (name, tags) {
-            var length = tags.length,
+        this.getDeployedTag = function (name, commits) {
+            var length = commits.length,
                 i = 0,
                 commit;
+            // Search for a commit named after the server
             for (i; i < length; i++) {
-                if (tags[i].name === 'server-' + name) {
-                    commit = tags[i].commit;
+                if (commits[i].name === 'server-' + name) {
+                    commit = commits[i].commit;
+                    break;
                 }
             }
+            // If non was found we cant tell whats currently deployed
             if (angular.isUndefined(commit)) {
-                return 'No deployed tag found.';
+                return 'No deployed commit found.';
             }
-            i = 0;
-            for (i; i < length; i++) {
-                if (tags[i].commit.id === commit.id && tags[i].name !== 'server-' + name) {
-                    return tags[i].type + ' ' + tags[i].name + ' - ' + commit.committerName + ': "' + commit.message + '"';
+            // If there was a commit found, we look if it matches a specific tag or branch and return the information
+            for (i = 0; i < length; i++) {
+                if (commits[i].commit.id === commit.id && commits[i].name !== 'server-' + name) {
+                    return commits[i].type + ' ' + commits[i].name + ' - ' + commit.committerName + ': "' + commit.message + '"';
                 }
             }
-            return commit.id + ' - ' + commit.committerName + ': "' + commit.message + '"';
+            // If no tag or branch matched the commit, we can at least
+            // return the sha1 of the commit along with the information
+            return 'sha1: ' + $filter('limitTo')(commit.id, 8) + ' - ' + commit.committerName + ': "' + commit.message + '"';
         };
 
         /**
@@ -46,6 +56,13 @@
          * @returns {number}
          */
         this.byCommitDate = function (a, b) {
+            if (angular.isUndefined(a.commit) ||
+                angular.isUndefined(b.commit) ||
+                angular.isUndefined(a.commit.date) ||
+                angular.isUndefined(b.commit.date)
+            ) {
+                return -1;
+            }
             if (a.commit.date < b.commit.date) {
                 return 1;
             }
---
+++ Resources/Public/Scripts/SurfCaptainApp/Tests/Unit/Services/UtilityServiceSpec.js
@@ -0,0 +1,97 @@
+/*global describe,beforeEach,module,it,expect,inject,angular,spyOn*/
+
+describe('UtilityService', function () {
+    'use strict';
+
+    var UtilityService, objectA, objectB, commits = [], commitA;
+
+    // Load the module
+    beforeEach(function () {
+        module('surfCaptain');
+        inject(function (_UtilityService_) {
+            UtilityService = _UtilityService_;
+        });
+    });
+
+    describe('->byCommitDate()', function () {
+
+        beforeEach(function () {
+            objectA = {
+                commit: {
+                    date: '2014-11-06T15:19:51.000+01:00'
+                }
+            };
+            objectB = {
+                commit: {
+                    date: '2014-11-07T15:19:51.000+01:00'
+                }
+            };
+        });
+
+        it('should be defined.', function () {
+            expect(UtilityService.byCommitDate).toBeDefined();
+        });
+
+        it('should return -1 if one object misses a commit property.', function () {
+            expect(UtilityService.byCommitDate({}, objectB)).toEqual(-1);
+            expect(UtilityService.byCommitDate(objectA, {})).toEqual(-1);
+        });
+
+        it('should return -1 if one object misses a commit.date property.', function () {
+            expect(UtilityService.byCommitDate({commit:{}}, objectB)).toEqual(-1);
+            expect(UtilityService.byCommitDate(objectA, {commit:{}})).toEqual(-1);
+        });
+
+        it('should return 1 if date of second object is greater than date of first object.', function () {
+            expect(UtilityService.byCommitDate(objectA, objectB)).toEqual(1);
+        });
+
+        it('should return -1 if date of second object is less than date of first object.', function () {
+            objectB.commit.date = '2014-11-02T15:19:51.000+01:00';
+            expect(UtilityService.byCommitDate(objectA, objectB)).toEqual(-1);
+        });
+    });
+
+    describe('->getDeployedTag()', function () {
+
+        beforeEach(function () {
+            commitA = {
+                name: 'server-foo',
+                type: 'Bar',
+                commit: {
+                    id: '12345',
+                    committerName: 'John Doe',
+                    message: '[TASK] some task'
+                }
+            };
+            commits.push(commitA);
+        });
+
+        it('should be defined.', function () {
+            expect(UtilityService.getDeployedTag).toBeDefined();
+        });
+
+        it('should return "No deployed commit found." if no matching name was found.', function () {
+            expect(UtilityService.getDeployedTag('bar', commits)).toEqual('No deployed commit found.');
+        });
+
+        it('should return commit information if a matching commit was found with different name again.', function () {
+            var commitB = angular.copy(commitA);
+            commitB.name = 'foo';
+            commits.push(commitB);
+            expect(UtilityService.getDeployedTag('foo', commits)).toEqual(commitB.type + ' ' + commitB.name + ' - ' + commitB.commit.committerName + ': "' + commitB.commit.message + '"');
+        });
+
+        it('should return commit information if a matching commit was not found with different name again.', function () {
+            var commitB = angular.copy(commitA);
+            commitB.commit.id = '23456';
+            commits.push(commitB);
+            expect(UtilityService.getDeployedTag('foo', commits)).toEqual('sha1:' + ' ' + commitA.commit.id + ' - ' + commitA.commit.committerName + ': "' + commitA.commit.message + '"');
+        });
+
+        afterEach(function () {
+            commits = [];
+        });
+    });
+
+});
