09edc9bc7df674d9b9e606d2ed9f66afa85fc0b9
Daniel Goerz <dlg@lightwerk.com>
2014-11-09 11:53:45 +0100

    [TASK] raised version to 1.0.11

--- Resources/Public/Scripts/SurfCaptainApp/SurfCaptainApp.js
+++ Resources/Public/Scripts/SurfCaptainApp/SurfCaptainApp.js
@@ -6,7 +6,7 @@
         .module('surfCaptain', ['ngRoute', 'xeditable', 'ngAnimate', 'ngMessages', 'ngBiscuit', 'toaster'])
         .config(routeConfiguration)
         .config(toasterConfiguration)
-        .value('version', '1.0.10')
+        .value('version', '1.0.11')
         .constant('CONFIG', {
             applicationTypes: {
                 deploy: 'Deploy',
