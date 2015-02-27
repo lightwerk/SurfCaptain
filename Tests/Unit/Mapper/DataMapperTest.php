699e0a4bae50ec1cc2762a19041414373fad8996
Daniel Goerz <dlg@lightwerk.com>
2015-02-27 10:09:46 +0100

    [TASK] added unit tests for \Lightwerk\SurfCaptain\Tests\Unit\Mapper\DataMapperTest

---
+++ Tests/Unit/Mapper/DataMapperTest.php
@@ -0,0 +1,116 @@
+<?php
+namespace Lightwerk\SurfCaptain\Tests\Unit\Mapper;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "Lightwerk.SurfCaptain". *
+ *                                                                        *
+ *                                                                        */
+
+use MyProject\Proxies\__CG__\stdClass;
+use TYPO3\Flow\Tests\UnitTestCase;
+
+/**
+ * DriverCompositeTest
+ *
+ * @package Lightwerk\SurfCaptain
+ * @author Daniel Goerz <dlg@lightwerk.com>
+ */
+class DataMapperTest extends UnitTestCase {
+
+	/**
+	 * @test
+	 * @return void
+	 */
+	public function getModelNameReturnsLastPartOfClassNameAsString() {
+		$dataMapperMock = $this->getAccessibleMock('Lightwerk\SurfCaptain\Mapper\DataMapper', array('foo'));
+		$object = new \Lightwerk\SurfCaptain\Tests\Unit\Mapper\DataMapperTest();
+		$className = $dataMapperMock->_call('getModelName', $object);
+		$this->assertSame('DataMapperTest', $className);
+	}
+
+
+	/**
+	 * @test
+	 * @return void
+	 */
+	public function getPropertyValueReturnsKey() {
+		$dataMapperMock = $this->getAccessibleMock('Lightwerk\SurfCaptain\Mapper\DataMapper', array('getModelName'));
+		$settings = array(
+			'model' => array(
+				'property' => '{{_KEY_}}'
+			)
+		);
+		$keyValue = 'keyValue';
+		$dataMapperMock->expects($this->once())->method('getModelName')->will($this->returnValue('model'));
+		$value = $dataMapperMock->_call('getPropertyValue', new \stdClass(), 'property', array(), $settings, $keyValue);
+		$this->assertSame($keyValue, $value);
+	}
+
+	/**
+	 * @return void
+	 * @test
+	 * @dataProvider getPropertyDataProvider
+	 */
+	public function getPropertyReturnsValueAfterMappingWasPerformed($objectData, $settings, $expectation) {
+		$dataMapperMock = $this->getAccessibleMock('Lightwerk\SurfCaptain\Mapper\DataMapper', array('getModelName'));
+		$dataMapperMock->expects($this->once())->method('getModelName')->will($this->returnValue('model'));
+		$value = $dataMapperMock->_call('getPropertyValue', new \stdClass(), 'myProperty', $objectData, $settings, 'keyValue');
+		$this->assertSame($expectation, $value);
+	}
+
+	/**
+	 * Data provider for getPropertyReturnsValueAfterMappingWasPerformed
+	 *
+	 * @return array
+	 */
+	public function getPropertyDataProvider() {
+		return array(
+			'Mapping simple property of model.' => array(
+				// $objectData
+				array (
+					'some_mapping' => 'fooBar'
+				),
+				// $settings
+				array(
+					'model' => array(
+						'myProperty' => '{{some_mapping}}'
+					)
+				),
+				// $expectation
+				'fooBar'
+			),
+			'Mapping array in property of model.' => array(
+				array (
+					'some_mapping' => 'fooBar',
+					'some_other_mapping' => 'foo'
+				),
+				array(
+					'model' => array(
+						'myProperty' => array(
+							'propertyA' => '{{some_mapping}}',
+							'propertyB' => '{{some_other_mapping}}'
+						)
+					)
+				),
+				array(
+					'propertyA' => 'fooBar',
+					'propertyB' => 'foo'
+				)
+			),
+			'No Mapping. Key is found in $objectData' => array(
+				array (
+					'myProperty' => 'fooBar',
+				),
+				array(),
+				'fooBar'
+			),
+			'No Mapping. Transformed key is found in $objectData' => array(
+				array (
+					'my_property' => 'fooBar',
+				),
+				array(),
+				'fooBar'
+			)
+		);
+	}
+}
